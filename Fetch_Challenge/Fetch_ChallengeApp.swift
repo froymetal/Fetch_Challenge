18a42ca14a1f08d0d7554e47960ed740bb997220
froymetal <froylan10@hotmail.com>
2025-01-09 22:13:35 -0600

    Initial Commit

---
+++ Fetch_Challenge/Fetch_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ChallengeApp.swift
+//  Fetch_Challenge
+//
+//  Created by Consultant on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
